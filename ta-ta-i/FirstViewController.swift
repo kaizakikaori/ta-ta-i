3e11f1708ba5280b9fc87eec51a9f7cd57a9227d
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-19 18:13:11 +0900

    first commit

---
+++ ta-ta-i/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ta-ta-i
+//
+//  Created by VERTEX21 on 2019/08/19.
+//  Copyright © 2019 k-kougi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ta-ta-i/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ta-ta-i
+//
+//  Created by VERTEX21 on 2019/08/19.
+//  Copyright © 2019 k-kougi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
